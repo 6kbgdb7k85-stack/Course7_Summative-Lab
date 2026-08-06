39f7e0016ad2979c547635e6feb379ea3cbe5f22
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-08-06 17:07:43 -0400

    basic model framwork complete

--- models/project.py
+++ models/project.py
@@ -1,4 +1,37 @@
-
-
 class Project:
-    pass
+    def __init__(self, name, due_date):
+        self.name = name
+        self.tasks = []
+        self.users = []
+        self.completed = False
+        self.due_date = due_date
+
+    def add_user(self,user):
+        if user in self.users:
+            print(f"User '{user}' already assigned to this project.")
+        else:
+            self.users.append(user)
+
+    def complete_project(self):
+        self.completed = True
+
+    def add_task(self,task):
+        if task in self.tasks:
+            print(f"Task '{task}' already exists")
+        else:
+            self.tasks.append(task)
+
+    def assign_task(self,task,user):
+        if user not in self.users:
+            while True:
+                should_add = input(f"User '{user}' is not assigned to this project. Add them? [y/n]: ")
+                if should_add.lower() == 'y':
+                    self.add_user(user)
+                    break
+                elif should_add.lower() == 'n':
+                    print(f"Task '{task}' not assigned to user '{user}'")
+                    return
+                else:
+                    print("Please enter 'y' or 'n'")
+        #TODO fetch task from data and attach
+        print(f"Task '{task}' assigned to '{user}'")
--- models/task.py
+++ models/task.py
@@ -1,4 +1,19 @@
 
 
 class Task:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self.completed = False
+        self.assigned_to = None
+
+    def complete_task(self):
+        self.completed = True
+        print(f"Task {self.title} completed.")
+
+    @property
+    def assigned_to(self):
+        return self._assigned_to
+    @assigned_to.setter
+    def assigned_to(self,value):
+        #TODO validation
+        self._assigned_to = value
